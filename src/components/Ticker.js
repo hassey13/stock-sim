2917e1a906e64d0c37de35b85aa482ea3fcd6dd1
hassey13 <ehasselbring@gmail.com>
2017-04-19 13:00:15 -0400

    updated color of background depending on stocks performance

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,17 +1,20 @@
 import React from 'react'
 
-const Ticker = ( props ) => (
+const Ticker = ( props ) => {
+  const tickerClass = props.stock.price - props.stock.open >= 0 ? 'ticker green' : 'ticker red'
 
-  <div className='ticker'>
-    <div className='ticker-price'>
-      <p>{ props.stock.price }</p>
+  return (
+    <div className={ tickerClass }>
+      <div className='ticker-price'>
+        <p>{ props.stock.price }</p>
+      </div>
+      <div className='ticker-price-change'>
+        <p className='larger'>{ (props.stock.price - props.stock.open).toFixed(2) }</p>
+        <p>{ `${((props.stock.price - props.stock.open)/ props.stock.open * 100).toFixed(2)}%` }</p>
+      </div>
+      <div className='clear'></div>
     </div>
-    <div className='ticker-price-change'>
-      <p className='larger'>{ (props.stock.price - props.stock.open).toFixed(2) }</p>
-      <p>{ `${((props.stock.price - props.stock.open)/ props.stock.open * 100).toFixed(2)}%` }</p>
-    </div>
-    <div className='clear'></div>
-  </div>
-)
+  )
+}
 
 export default Ticker
